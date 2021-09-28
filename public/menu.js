a1954f841f88cf3a33395db9dc9d171932cc4223
fcventura02 <filipecvr4@gmail.com>
2021-09-27 21:24:13 -0300

    Created menu hamburguer

---
+++ public/menu.js
@@ -0,0 +1,43 @@
+const bars = document.getElementById("bars");
+const menu = document.getElementById("menu");
+
+bars.addEventListener("click", () => {
+  if (menu.classList.contains("hidden") && window.innerWidth < 768) {
+    menu.classList.toggle("hidden");
+    menu.classList.add(
+      "flex",
+      "flex-col",
+      "text-center",
+      "w-full",
+      "absolute",
+      "top-16",
+      "bg-gray-300"
+    );
+  } else {
+    menu.classList.toggle("hidden");
+    menu.classList.remove(
+      "flex",
+      "flex-col",
+      "text-center",
+      "w-full",
+      "absolute",
+      "top-16",
+      "bg-gray-300"
+    );
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menu.classList.add("hidden");
+    menu.classList.remove(
+      "flex",
+      "flex-col",
+      "text-center",
+      "w-full",
+      "absolute",
+      "top-16",
+      "bg-gray-300"
+    );
+  }
+});
